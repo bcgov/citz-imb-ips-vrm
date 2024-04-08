de4cce962522c62212f790f9307768d23e33a4d8
Brandon Bouchard <brandonjbouchard@gmail.com>
2024-04-08 15:32:36 -0700

    Change env names for compatibility with RHEL Postgres image.

--- server/plugins/vrmprocess.py
+++ server/plugins/vrmprocess.py
@@ -18,9 +18,9 @@ try:
     JIRA_API_USERNAME = config("JIRA_API_USERNAME") # Username for JIRA API
     JIRA_API_KEY = config("JIRA_API_KEY") # Key for JIRA API authentication
     PG_HOST = config("POSTGRES_HOST") # PostgreSQL host
-    PG_DBNAME = config("POSTGRES_DB") # PostgreSQL database name
-    PG_USER = config("POSTGRES_USER") # PostgreSQL username
-    PG_PASSWORD = config("POSTGRES_PASSWORD") # PostgreSQL password
+    PG_DBNAME = config("POSTGRESQL_DATABASE") # PostgreSQL database name
+    PG_USER = config("POSTGRESQL_USER") # PostgreSQL username
+    PG_PASSWORD = config("POSTGRESQL_PASSWORD") # PostgreSQL password
     PG_PORT = config("POSTGRES_PORT", default=5432, cast=int) # PostgreSQL port, default is 5432
     TENABLE_APIKEY = config("TENABLE_API_KEY") # Tenable API key, default is empty string
     client_encoding = config("CLIENT_ENCODING", default='utf-8') # Client encoding, default is utf-8
